9a949d8f8d88cc954d120e4af789500a98c06ea8
Katrin <alimovaea@sibur.ru>
2022-03-28 14:24:07 +0300

    stats service refactor

--- stats-service/src/stats/stats.controller.ts
+++ stats-service/src/stats/stats.controller.ts
@@ -1,5 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
+import { IEmailStats } from 'src/const';
 import { StatsService } from '../stats/stats.service';
 
 @Controller('posts')
@@ -7,8 +8,8 @@ export class StatsController {
   constructor(private readonly statsService: StatsService) {}
 
   @MessagePattern('send.email.stats')
-  getStats(@Payload() stats) {    
-    console.log('Stats recieved');
-    return this.statsService.getStats(stats);
+  getStats(@Payload() stats: { value : IEmailStats}) {    
+    const { value } = stats
+    return this.statsService.getStats(value);
   }
 }
